c11a6b2411d17af52a0823f1d9a4a9b3f16df989
lokidark022 <joshuacompendio25@gmail.com>
2025-03-06 20:28:01 +0800

    jwt auth added

--- server.js
+++ server.js
@@ -2,8 +2,20 @@ const express = require('express')
 const mysql = require('mysql')
 const cors = require('cors')
 const path = require('path')
-
+const jwt = require("jsonwebtoken")
 const app = express()
+
+
+
+
+
+
+
+
+
+
+
+
 
 app.use(express.static(path.join(__dirname, "public")))
 app.use(cors())
@@ -18,20 +30,143 @@ const db = mysql.createConnection({
     database: "react_db"
 })
 
+
+const users = [
+    {
+        id: 1,
+        username: "admin",
+        password: "admin"
+    },
+    {
+        id: 2,
+        username: "admin2",
+        password: "admin2"
+    }
+];
+
+let refreshTokens = [];
+app.post("/refresh", (req,res) => {
+    const refreshToken = req.body.token;
+
+    if(!refreshToken) return res.status(401).json("You are not authenticated!");
+    if(!refreshTokens.includes(refreshToken)){
+        return res.status(403).json("Refresh token is not valid");
+    }
+    jwt.verify(refreshToken, "myRefreshSecretKey", (err,user) =>{
+        err && console.log(err);
+        refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+
+        const newAccessToken = generateAccessToken(user);
+        const newRefreshToken = generateRefrestToken(user);
+
+        refreshTokens.push(newRefreshToken);
+        res.status(200).json({
+            accessToken: newAccessToken,
+            refreshToken:  newRefreshToken
+        })
+    })
+
+
+})
+
+
+const verify = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if(authHeader){
+        const token = authHeader.split(" ")[1];
+
+        jwt.verify(token, "mySecretKey", (err, user) => {
+            if(err){
+                return res.status(403).json("Token is not valid");
+            }
+
+            req.user = user;
+            next();
+        });
+    }else {
+        res.status(401).json("You are not authenticated" );
+    }
+};
+
+
+app.delete("/users/:userId",verify,(req,res) => {
+    res.status(200).json("user action valid");
+})
+
+
+const generateAccessToken = (user) => {
+    return jwt.sign({id: user.id,username:user.username}, "mySecretKey", { expiresIn: "30s"});
+};
+
+const generateRefrestToken = (user) => {
+    return jwt.sign({id: user.id,username:user.username}, "myRefreshSecretKey");
+}
+
+app.post('/login', (req, res) => {
+   
+
+    // const values = {
+    //     req.body.username,
+    //     req.body.password,
+    //    }
+    const user = users.find((u) => {
+        return u.username === req.body.username && u.password === req.body.password;
+    });
+    if(user){
+        // res.json(user)
+        // const accessToken = jwt.sign({id: user.id,username:user.username}, "mySecretKey", { expiresIn: "15m"});
+        const accessToken =  generateAccessToken(user);
+        const refreshToken =  generateRefrestToken(user);
+
+        refreshTokens.push(refreshToken);
+        res.json({
+            username:user.username,
+            accessToken,
+            refreshToken
+        });
+    }else{
+        res.status(400).json("username or password incorrect!" );
+        
+    }
+
+//    res.json("its works")
+
+})
+
+
 app.post('/',(req,res) =>{
     const values = [
         req.body.email,
         req.body.password,
     ]
-    const sql = "SELECT * FROM users WHERE email =? AND password =?";
+    const sql = "SELECT email FROM users WHERE email =? AND password =?";
   
     db.query(sql,values, (err, result) => {
         if(result.length > 0){
-            res.json({success: 'valid'})
+            //  res.json({success: 'valid'})
+            
+            const accessToken =  generateAccessToken(result);
+            const refreshToken =  generateRefrestToken(result);
+    
+            refreshTokens.push(refreshToken);
+
+
+            // res.json({success: accessToken});
+            res.json({
+                isValid:true,
+                tmessage:'Valid User',
+                bmessage:'Valid Credentials',
+                email:req.body.email,
+                accessToken,
+                refreshToken
+            });
+
            
         }else{
-            res.json({success: 'invalid'})
-            // res.json({message: 'Something unexpected has occured' + err})
+            // res.json({success: 'invalid'})
+
+            res.json({tmessage: "Invalid User",bmessage:"Incorrect email or password please try again.",isValid:false});
+         
         }
 
         // if(result != null) return res.json({message: 'Something unexpected has occured' + err})
@@ -39,6 +174,15 @@ app.post('/',(req,res) =>{
     })
 
 })
+
+
+app.post("/logout", verify, (req,res) => {
+    const refreshToken = req.body.token;
+    refreshTokens = refreshTokens.filter((token) => token !== refreshToken);
+    res.status(200).json("You logged out successfully.");
+});
+
+
 
 app.listen(port, ()=>{
     console.log('listening... PORT:',port)
